c6958e25921f2117ef6e29e6f7498fad5099574c
Jonathan Adalin <jonathan.adalin@mail.mcgill.ca>
2015-11-03 18:58:13 -0500

    Standardizing comments for API use in Coordinate Class

--- src/ev3Navigator/Coordinate.java
+++ src/ev3Navigator/Coordinate.java
@@ -1,22 +1,35 @@
 package ev3Navigator;
 
 
-//Simple class that creates an object out of the given coordinates
+/**
+ * Creates creates an object that stores x and y coordinates.
+ */
 public class Coordinate {
 	
 	private double X;
 	private double Y;
 	
+	/**
+	 * Store coordinates in the coordinate object
+	 * @param pX The x coordinate
+	 * @param pY The y coordinate
+	 */
 	public Coordinate(double pX, double pY)
 	{
 		X = pX;
 		Y = pY;
 	}
-
+	/**
+	 * Get the x coordinate
+	 * @return the x coordinate
+	 */
 	public double getX() {
 		return X;
 	}
-
+	/**
+	 * Get the y coordinate
+	 * @return the y coordinate
+	 */
 	public double getY() {
 		return Y;
 	}
